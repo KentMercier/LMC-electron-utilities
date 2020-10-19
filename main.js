75cf6601ac87acd9464d0c9b56a71c20a4d75099
Kent Mercier <kentmercier@gmail.com>
2020-10-18 23:32:30 -0500

    added tray and testing links.

--- main.js
+++ main.js
@@ -29,11 +29,30 @@ function createWindow() {
     win = null;
   })
 }
+function createBrowserWindow() {
+  win = new BrowserWindow(({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  }));
+  // and load the index.html of the app.
+
+
+    win.loadURL(`https://landmancentral.com`);
+
+
+  win.on('closed', () => {
+    win = null;
+  })
+}
 
 app.on('ready', createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
+
   }
 })
 
@@ -46,9 +65,11 @@ app.on('activate', () => {
 app.whenReady().then(() => {
   tray = new Tray('./src/favicon.ico')
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
+    { label: 'Visit LandmanCentral.com', click: function ()  {
+      createBrowserWindow()
+      } },
     { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Exit LMC Tray', type: 'radio'},
     { label: 'Item4', type: 'radio' }
   ])
   tray.setToolTip('This is my application.')
